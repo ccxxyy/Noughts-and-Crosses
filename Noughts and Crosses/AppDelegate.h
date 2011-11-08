537c2b0a25b69952ca8ba9eb8fb07cc4f3b7b8a9
Xiaoyi Cai <sawyer_cai@Xiaoyis-MacBook-Pro.local>
2011-11-08 18:12:24 +0000

    Initial Commit

---
+++ Noughts and Crosses/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Noughts and Crosses
+//
+//  Created by Xiaoyi Cai on 08/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Noughts and Crosses/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Noughts and Crosses
+//
+//  Created by Xiaoyi Cai on 08/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
